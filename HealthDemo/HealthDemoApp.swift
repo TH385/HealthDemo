1558022ddf3bdaa085dff8423986d166267cc8e0
author <author@example.com>
2025-06-22 10:22:08 +0800

    Initial Commit

---
+++ HealthDemo/HealthDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDemoApp.swift
+//  HealthDemo
+//
+//  Created by admin on 2025-06-22.
+//
+
+import SwiftUI
+
+@main
+struct HealthDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
